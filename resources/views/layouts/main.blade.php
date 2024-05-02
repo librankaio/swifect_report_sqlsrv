2fb80e4c0455a3d50baa0e5e4b7fec9ee78484bb
librankaio <95515543+librankaio@users.noreply.github.com>
2024-05-02 10:33:45 +0700

    update bootstrap 02052024 v2

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,8 +11,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Swifect IT Inventory Online Bea Cukai</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> --}}
+  <link rel="stylesheet" href="{{ asset("../bootstrap/css/bootstrap.min.css") }}">
   <link href="/css/style.css" rel="stylesheet">
   <link href="/css/loading.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
@@ -69,9 +70,10 @@
   </div>
   <!-- END Content -->
   <!-- js importer -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+  {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-  </script>
+  </script> --}}
+  <script src="{{ asset("../bootstrap/js/bootstrap.bundle.min.js") }}"></script>
   <!-- END js importer -->
 
   <script type="text/javascript">
